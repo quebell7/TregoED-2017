06959ffc6f0827c565852332d668ab4bf1aab898
quebell7 <kbell.marinescu@gmail.com>
2016-11-30 13:21:17 -0800

    trying again

---
+++ js/index.js
@@ -0,0 +1,72 @@
+$(document).ready(pageReady);
+
+function pageReady() {
+
+	$(".dropbtn").hover(function showRollover() {
+		 event.preventDefault();
+    $(this).addClass("dropdown-content");
+    // $(this).show("thickline");
+
+ }, function removeRollover() {
+ 		 event.preventDefault();
+    $(this).removeClass("rollover");
+} );
+
+		// Set up click listener on mobile nav button
+	$(".nav-toggle").click(toggleMobileNav);
+
+	function toggleMobileNav() {
+
+		// Toggle sliding the mobile nav container to reveal/hide
+		$(".nav-menu").slideToggle();
+
+		// Toggle applying class to the hamburger lines to trigger animation
+		// $(".line").toggleClass("active");
+		$(".circle").toggle();
+		    if ($(window).width() > 768) {
+      	$(".icon").toggle();
+    	}
+	}
+}
+	
+
+///nav links
+
+// $(".navline").hover(function showRollover() {
+// 		 event.preventDefault();
+//     $(this).addClass("rollover");
+//     // $(this).show("thickline");
+
+//  }, function removeRollover() {
+//  		 event.preventDefault();
+//     $(this).removeClass("rollover");
+// } );
+
+
+// $(".nav-toggl").click(mobileNavToggle);
+	
+// 	function mobileNavToggle() {
+// 		$(".nav-menu").slideToggle();
+// 		$(".line").toggleClass("active");
+// 		$("#logo").toggle();
+// 		    if ($(window).width() > 768) {
+//       	$(".orders").toggle();
+//     	}
+// 	}
+
+// }
+
+
+
+
+// function clickHandler() {
+//     $('#hide').toggle();
+//     $('#show').toggle();
+//     $('.orders').toggle();
+// }
+    
+// $(document).ready(function(){
+//     $('#show').hide();
+    
+//     $('#hide, #show').on('click', clickHandler);
+// });
