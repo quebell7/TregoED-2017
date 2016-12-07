5ce2e793ca9376a8af7a0f3a67e092c4185eab6f
quebell7 <kbell.marinescu@gmail.com>
2016-12-06 21:21:42 -0800

    trying out background animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
+// Initial the heroIndex to 0
+var heroIndex = 0;
+
+// The array of strings corresponding to the source location of your images; you can also use URLs pointing to your project here too
+var heroImages = [
+  'http://manninghamvet.com.au/sites/default/files/styles/large/public/images/article/cats.jpg',
+  'https://static1.squarespace.com/static/50c8c55be4b0a1d4330d16f8/t/5577357ae4b000535469c1c1/1401647944177/Black-White-Cat-Kitten-Wallpaper-Desktop.jpg',
+  'http://www.catprotection.com.au/wp-content/uploads/2014/11/5507692-cat-m.jpg',
+  'https://www.petfinder.com/wp-content/uploads/2013/03/101888324-632x353-flipped.jpg'
+];
+
 $(document).ready(pageReady);
 
+// This updates the index 
+function updateHeroIndex() {
+  heroIndex++;
+
+  // A bit of conditional logic to reset to 0
+  if (heroIndex >= heroImages.length) heroIndex = 0;
+}
+
+// This function handles the updating of the hero using the array of source strings we defined at the top and the heroIndex
+function updateHero() {
+  var heroSource = heroImages[heroIndex];
+  var backgroundUrl = 'url(' + heroSource + ')';
+  $('.hero').css('background-image', backgroundUrl);
+  updateHeroIndex();
+}
+
 function pageReady() {
+
+	 updateHero();
+  
+  // This is how to repeatedly call the updateHero function on a timer
+  // This will call our updateHero() function every 1000 milliseconds (1 second)
+  setInterval(updateHero, 5000);
+}
 
 	$(".dropbtn").hover(function showRollover() {
 		 event.preventDefault();
@@ -39,7 +73,7 @@ function pageReady() {
 			$(".nav-menu").hide();
 		}
 	}
-}
+
 	
 
 
