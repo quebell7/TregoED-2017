25b7eb6b29a428d2d833c72f5611f3edc2082f49
quebell7 <kbell.marinescu@gmail.com>
2016-12-09 11:56:41 -0800

    tweak

--- js/index.js
+++ js/index.js
@@ -25,7 +25,8 @@ function updateHeroIndex() {
 function updateHero() {
   var heroSource = heroImages[heroIndex];
   var backgroundUrl = 'url(' + heroSource + ')';
-  $('.hero').css('background-image', ".is-large", backgroundUrl);
+  $('.hero').css('background-image', backgroundUrl);
+  $('.is-large').show();
   updateHeroIndex();
 }
 
